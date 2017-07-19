6c6598231ffcd53594298284ddc41b1989da5965
Super.Li <xupenglee@gmail.com>
2017-07-19 14:30:40 +0800

    1. implement maven-resources-plugin to switch evnvironment 2. inherit logback configuration from parent spring-boot

--- web/src/main/java/org/lxp/springboot/controller/VersionController.java
+++ web/src/main/java/org/lxp/springboot/controller/VersionController.java
@@ -8,6 +8,8 @@ import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class VersionController {
+    private static final Logger LOG = LoggerFactory.getLogger(VersionController.class);
     private static final ThreadLocal<DateFormat> DATE_FORMAT = new ThreadLocal<DateFormat>() {
         public DateFormat initialValue() {
             return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -28,9 +31,10 @@ public class VersionController {
     @Value("${project.format}")
     private String format;
 
-    @RequestMapping(value = "version", method = GET)
+    @RequestMapping(value = "/version", method = GET)
     @ApiOperation(value = "查看版本信息")
     public Map<String, String> version() throws ParseException {
+        LOG.info("version接口被调用！");
         Map<String, String> map = new HashMap<String, String>();
         map.put("version", version);
         map.put("builtAt", DATE_FORMAT.get().format(new SimpleDateFormat(format).parse(builtAt)));
