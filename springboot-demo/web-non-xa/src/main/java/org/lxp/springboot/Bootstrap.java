e7cd65915560a1cb68097100fde0338e487f3367
Super Li <xupenglee@gmail.com>
2018-05-13 18:36:11 +0800

    rename all launch classes' name to Bootstrap

--- springboot-demo/web-non-xa/src/main/java/org/lxp/springboot/Bootstrap.java
+++ springboot-demo/web-non-xa/src/main/java/org/lxp/springboot/Bootstrap.java
@@ -22,7 +22,7 @@ import org.springframework.web.filter.DelegatingFilterProxy;
 
 /** disable auto web security **/
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class WebApplication {
+public class Bootstrap {
     @Value("${web.swagger.enabled}")
     private boolean swaggerAllowed;
 
@@ -55,6 +55,6 @@ public class WebApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
+        SpringApplication.run(Bootstrap.class, args);
     }
 }
