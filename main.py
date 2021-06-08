0a92ba090c3a4da54e0f99ef8e1c803dd5240aea
Super Li <xupenglee@gmail.com>
2021-06-09 01:23:59 +0800

    initialize a pandas test

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+def print_hi():
+    # Use a breakpoint in the code line below to debug your script.
+    print(pd.Series([1, 3, 5, np.nan, 6, 8]))  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
