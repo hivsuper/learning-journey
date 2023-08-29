e18fb2f896d968db75f3f8d8986af3234c361055
Super Li <xupenglee@gmail.com>
2023-08-30 01:45:52 +0800

    upload STUDY-CSHARP

---
+++ STUDY-CSHARP/Utility.Tests/HelperTest.cs
@@ -0,0 +1,68 @@
+namespace Utility.Tests;
+
+public class HelperTest
+{
+    [Fact]
+    public void TestBoolTest()
+    {
+        Assert.False(Program.BoolTest(null));
+        Assert.True(Program.BoolTest(true));
+        Assert.False(Program.BoolTest(false));
+    }
+
+    [Fact]
+    public void TestIsListNotNullOrEmpty()
+    {
+        Assert.False(Program.IsListNotNullOrEmpty(null));
+        Assert.False(Program.IsListNotNullOrEmpty(new List<string> { }));
+        Assert.True(Program.IsListNotNullOrEmpty(new List<string> { "aa" }));
+    }
+
+    [Fact]
+    public void TestRemveNonAlphanumeric()
+    {
+        Assert.Null(Program.RemveNonAlphanumeric(null));
+        Assert.Equal("Send  Reply 1 2", Program.RemveNonAlphanumeric("Send & Reply 1 2"));
+        Assert.Equal("\r\n\t", Program.RemveNonAlphanumeric("-&./,;'\"!@#$%^&*()\r\n\t"));
+        Assert.Equal("ABCDEDFGHIJKLMNOPQRSTUVWXYZ0123456789abcdedfghijklmnopqrstuvwxyz", Program.RemveNonAlphanumeric("ABCDEDFGHIJKLMNOPQRSTUVWXYZ0123456789abcdedfghijklmnopqrstuvwxyz"));
+    }
+
+    [Fact]
+    public void TestIsNumeric()
+    {
+        Assert.False(Program.IsNumeric("Send & Reply 1 2"));
+        Assert.True(Program.IsNumeric("132432"));
+        Assert.True(Program.IsNumeric("43214123 "));
+    }
+
+    [Fact]
+    public void TestAssetType()
+    {
+        Assert.Equal(AssetType.AFund, (AssetType)1);
+        Assert.Equal(AssetType.HFund, (AssetType)2);
+        Assert.Equal(AssetType.PFund, (AssetType)3);
+
+        Assert.True(Enum.IsDefined(typeof(AssetType), 1));
+    }
+
+    [Fact]
+    public void TestTestEnum()
+    {
+        Array array = Enum.GetNames(typeof(TestEnum));
+        Assert.Equal("AnB", array.GetValue(0)?.ToString());
+        Assert.Equal(TestEnum.AnB, Enum.Parse(typeof(TestEnum), "AnB"));
+        Assert.Equal(TestEnum.isCD, Enum.Parse(typeof(TestEnum), "isCD"));
+        Assert.Equal(TestEnum.INCLUDEE, Enum.Parse(typeof(TestEnum), "INCLUDEE"));
+        Assert.Equal(TestEnum.R, Enum.Parse(typeof(TestEnum), "R"));
+    }
+
+    [Fact]
+    public void TestTestEnumException()
+    {
+        Action testCode = () => { Enum.Parse(typeof(TestEnum), "AnB1 ", true); };
+        var ex = Record.Exception(testCode);
+        Assert.NotNull(ex);
+        Assert.Equal("Requested value 'AnB1 ' was not found.", ex.Message);
+        Assert.IsType<System.ArgumentException>(ex);
+    }
+}
---
+++ STUDY-CSHARP/Utility/Helper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Utility
+{
+    public class Program
+    {
+        public static bool BoolTest(bool? input)
+        {
+            return input ?? false;
+        }
+
+        public static bool IsListNotNullOrEmpty(List<string>? list)
+        {
+            return list?.Count > 0;
+        }
+
+        public static string? RemveNonAlphanumeric(string? input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+            System.Text.RegularExpressions.Regex rgx = new System.Text.RegularExpressions.Regex(@"[^a-zA-Z\d\s]");
+            return rgx.Replace(input, "");
+        }
+
+        public static bool IsNumeric(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            System.Text.RegularExpressions.Regex rgx = new System.Text.RegularExpressions.Regex(@"^\d+$");
+            return rgx.IsMatch(new System.Text.RegularExpressions.Regex(@"\s").Replace(input, ""));
+        }
+    }
+
+    public enum AssetType
+    {
+        [Description("11")]
+        AFund = 1,
+        [Description("22")]
+        HFund = 2,
+        [Description("33")]
+        PFund = 3
+    }
+
+    public enum TestEnum
+    {
+        [Description("A&B")]
+        AnB,
+        [Description("C & D")]
+        isCD,
+        [Description("Include E")]
+        INCLUDEE,
+        [Description("R")]
+        R
+    }
+}
