61061cb2baaac9b1ca30a763e9e4fcb46d7aa49f
Super Li <xupenglee@gmail.com>
2023-10-25 00:44:58 +0800

    Add more tests of HttpClient according to https://www.baeldung.com/java-httpclient-timeout and https://www.mastertheboss.com/java/how-to-tune-the-performance-of-java-11-httpclients

--- web-crawler/src/main/java/org/lxp/crawler/JEP321HttpClientHelper.java
+++ web-crawler/src/main/java/org/lxp/crawler/JEP321HttpClientHelper.java
@@ -7,10 +7,27 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
 
 public class JEP321HttpClientHelper {
+
+    private ExecutorService executor;
+
+    public JEP321HttpClientHelper(String maxTotalConnection) {
+        System.setProperty("jdk.httpclient.connectionPoolSize", maxTotalConnection);
+    }
+
+    public JEP321HttpClientHelper(int maxTotalConnection) {
+        this.executor = Executors.newFixedThreadPool(maxTotalConnection);
+    }
+
     public String post(String url, String requestBody, String... headers) throws URISyntaxException, IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(url))
@@ -29,10 +46,38 @@ public class JEP321HttpClientHelper {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(url))
                 .headers(headers)
+                .version(HttpClient.Version.HTTP_2)
                 .GET().build();
         HttpResponse<String> response = HttpClient.newBuilder()
                 .build()
                 .send(request, HttpResponse.BodyHandlers.ofString());
         return response.body();
+    }
+
+    public String getWithThreadPool(String url, String... headers) throws URISyntaxException, IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(url))
+                .headers(headers)
+                .GET().build();
+        HttpResponse<String> response = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .executor(executor)
+                .build()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+    public String getAsync(HttpRequest request, HttpClient httpClient, int waitSeconds) throws InterruptedException, ExecutionException, TimeoutException {
+        CompletableFuture<String> response = httpClient
+                .sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .exceptionally(Throwable::getMessage);
+        return response.get(waitSeconds, TimeUnit.SECONDS);
+    }
+
+    public HttpClient getHttpClientWithTimeout(int seconds) {
+        return HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(seconds))
+                .build();
     }
 }
--- web-crawler/src/test/java/org/lxp/crawler/JEP321HttpClientHelperTest.java
+++ web-crawler/src/test/java/org/lxp/crawler/JEP321HttpClientHelperTest.java
@@ -1,6 +1,8 @@
 package org.lxp.crawler;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.matching.RegexPattern;
+import com.github.tomakehurst.wiremock.matching.UrlPattern;
 import org.apache.http.HttpStatus;
 import org.junit.After;
 import org.junit.Before;
@@ -8,7 +10,14 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpConnectTimeoutException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
@@ -16,15 +25,23 @@ import static com.github.tomakehurst.wiremock.client.WireMock.ok;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
 
 public class JEP321HttpClientHelperTest {
+    public static final String TEXT_PLAIN_CHARSET_UTF_8 = "text/plain;charset=UTF-8";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String RESULT = "hahaha";
     private final String crawlerUrl = "http://127.0.0.1:8080/crawler-test";
+    private final int maxTotalConnection = 10;
     @Rule
     public WireMockRule forecastIoService = new WireMockRule();
 
     @Before
     public void setUp() {
         forecastIoService.start();
+        forecastIoService.stubFor(get(new UrlPattern(new RegexPattern("/crawler-test(.*)"), true))
+                .withHeader(CONTENT_TYPE, containing(TEXT_PLAIN_CHARSET_UTF_8))
+                .willReturn(ok(RESULT).withFixedDelay(10).withStatus(HttpStatus.SC_OK)));
     }
 
     @After
@@ -34,23 +51,56 @@ public class JEP321HttpClientHelperTest {
 
     @Test
     public void testPost() throws URISyntaxException, IOException, InterruptedException {
-        JEP321HttpClientHelper helper = new JEP321HttpClientHelper();
-        final String result = "hahaha";
+        JEP321HttpClientHelper helper = new JEP321HttpClientHelper(String.valueOf(maxTotalConnection));
         final String requestBody = "lalala";
         forecastIoService.stubFor(post(urlEqualTo("/crawler-test"))
-                .withHeader("Content-Type", containing("text/plain;charset=UTF-8"))
+                .withHeader(CONTENT_TYPE, containing(TEXT_PLAIN_CHARSET_UTF_8))
                 .withRequestBody(containing(requestBody))
-                .willReturn(ok(result).withFixedDelay(10).withStatus(HttpStatus.SC_OK)));
-        assertThat(helper.post(crawlerUrl, requestBody, "Content-Type", "text/plain;charset=UTF-8")).isEqualTo(result);
+                .willReturn(ok(RESULT).withFixedDelay(10).withStatus(HttpStatus.SC_OK)));
+        assertThat(helper.post(crawlerUrl, requestBody, CONTENT_TYPE, TEXT_PLAIN_CHARSET_UTF_8)).isEqualTo(RESULT);
     }
 
     @Test
     public void testGet() throws URISyntaxException, IOException, InterruptedException {
-        JEP321HttpClientHelper helper = new JEP321HttpClientHelper();
-        final String result = "hahaha";
-        forecastIoService.stubFor(get(urlEqualTo("/crawler-test".concat("?p1=123")))
-                .withHeader("Content-Type", containing("text/plain;charset=UTF-8"))
-                .willReturn(ok(result).withFixedDelay(10).withStatus(HttpStatus.SC_OK)));
-        assertThat(helper.get(crawlerUrl.concat("?p1=123"), "Content-Type", "text/plain;charset=UTF-8")).isEqualTo(result);
+        JEP321HttpClientHelper helper = new JEP321HttpClientHelper(String.valueOf(maxTotalConnection));
+        assertThat(helper.get(crawlerUrl.concat("?p1=123"), CONTENT_TYPE, TEXT_PLAIN_CHARSET_UTF_8)).isEqualTo(RESULT);
+    }
+
+    @Test
+    public void getWithThreadPool() throws URISyntaxException, IOException, InterruptedException {
+        JEP321HttpClientHelper helper = new JEP321HttpClientHelper(maxTotalConnection);
+        assertThat(helper.getWithThreadPool(crawlerUrl, CONTENT_TYPE, TEXT_PLAIN_CHARSET_UTF_8)).isEqualTo(RESULT);
+    }
+
+    @Test
+    public void getAsync() throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException {
+        JEP321HttpClientHelper helper = new JEP321HttpClientHelper(maxTotalConnection);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(crawlerUrl))
+                .headers(CONTENT_TYPE, TEXT_PLAIN_CHARSET_UTF_8)
+                .GET().build();
+        assertThat(helper.getAsync(request, helper.getHttpClientWithTimeout(1), 5)).isEqualTo(RESULT);
+    }
+
+    @Test
+    public void getAsyncThrowExceptionWhenTimeout() throws InterruptedException, ExecutionException, TimeoutException {
+        JEP321HttpClientHelper helper = new JEP321HttpClientHelper(maxTotalConnection);
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://10.255.255.1")).GET().build();
+        String response = helper.getAsync(httpRequest, helper.getHttpClientWithTimeout(1), 5);
+        assertThat(response).contains("timed out");
+    }
+
+    @Test
+    public void getHttpClientWithTimeout() {
+        JEP321HttpClientHelper helper = new JEP321HttpClientHelper(maxTotalConnection);
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://10.255.255.1")).GET().build();
+        HttpClient httpClient = helper.getHttpClientWithTimeout(1);
+        HttpConnectTimeoutException thrown = assertThrows(
+                "Expected send() to throw HttpConnectTimeoutException, but it didn't",
+                HttpConnectTimeoutException.class,
+                () -> httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString()));
+        assertThat(thrown.getMessage()).contains("timed out");
     }
 }
