7230ce5e3e63c8de9be3647e1791ba97d91ecfa4
Super Li <xupenglee@gmail.com>
2023-10-27 00:06:41 +0800

    copy codes from https://www.baeldung.com/java-compress-and-uncompress

---
+++ develop-tools/src/main/java/com/lxp/tool/archive/ArchiveHelper.java
@@ -0,0 +1,149 @@
+package com.lxp.tool.archive;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+public class ArchiveHelper {
+    public void zip(String sourceFile, String targetFile) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(targetFile);
+             ZipOutputStream zipOut = new ZipOutputStream(fos)) {
+            File fileToZip = new File(sourceFile);
+            if (fileToZip.isDirectory()) {
+                zipFile(fileToZip, fileToZip.getName(), zipOut);
+            } else {
+                try (FileInputStream fis = new FileInputStream(fileToZip)) {
+                    ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+                    zipOut.putNextEntry(zipEntry);
+                    byte[] bytes = new byte[1024];
+                    int length;
+                    while ((length = fis.read(bytes)) >= 0) {
+                        zipOut.write(bytes, 0, length);
+                    }
+                }
+            }
+        }
+    }
+
+    public void zip(List<String> srcFiles, String targetFile) throws IOException {
+        try (final FileOutputStream fos = new FileOutputStream(targetFile);
+             ZipOutputStream zipOut = new ZipOutputStream(fos)) {
+            for (String srcFile : srcFiles) {
+                File fileToZip = new File(srcFile);
+                try (FileInputStream fis = new FileInputStream(fileToZip)) {
+                    ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+                    zipOut.putNextEntry(zipEntry);
+
+                    byte[] bytes = new byte[1024];
+                    int length;
+                    while ((length = fis.read(bytes)) >= 0) {
+                        zipOut.write(bytes, 0, length);
+                    }
+                }
+            }
+        }
+    }
+
+    public void zipAppend(String sourceFile, String targetFile) throws IOException {
+        File file = new File(sourceFile);
+        if (file.isDirectory()) {
+            throw new IllegalArgumentException(sourceFile + " is a directory");
+        }
+        Map<String, String> env = new HashMap<>();
+        env.put("create", "true");
+        Path path = Paths.get(targetFile);
+        URI uri = URI.create("jar:" + path.toUri());
+        try (FileSystem fs = FileSystems.newFileSystem(uri, env)) {
+            Path nf = fs.getPath(file.getName());
+            Files.write(nf, Files.readAllBytes(Path.of(sourceFile)), StandardOpenOption.CREATE);
+        }
+    }
+
+    public void unzip(String zipFile, String destinationDirectory) throws IOException {
+        File destDir = new File(destinationDirectory);
+        byte[] buffer = new byte[1024];
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile))) {
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                File newFile = newFile(destDir, zipEntry);
+                if (zipEntry.isDirectory()) {
+                    if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                        throw new IOException("Failed to create directory " + newFile);
+                    }
+                } else {
+                    // fix for Windows-created archives
+                    File parent = newFile.getParentFile();
+                    if (!parent.isDirectory() && !parent.mkdirs()) {
+                        throw new IOException("Failed to create directory " + parent);
+                    }
+
+                    // write file content
+                    try (FileOutputStream fos = new FileOutputStream(newFile)) {
+                        int len;
+                        while ((len = zis.read(buffer)) > 0) {
+                            fos.write(buffer, 0, len);
+                        }
+                    }
+                }
+                zipEntry = zis.getNextEntry();
+            }
+            zis.closeEntry();
+        }
+    }
+
+    private File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+        return destFile;
+    }
+
+    private void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
+        if (fileToZip.isHidden()) {
+            return;
+        }
+        if (fileToZip.isDirectory()) {
+            if (fileName.endsWith(File.separator)) {
+                zipOut.putNextEntry(new ZipEntry(fileName));
+                zipOut.closeEntry();
+            } else {
+                zipOut.putNextEntry(new ZipEntry(fileName + File.separator));
+                zipOut.closeEntry();
+            }
+            File[] children = fileToZip.listFiles();
+            if (Optional.ofNullable(children).isPresent()) {
+                for (File childFile : children) {
+                    zipFile(childFile, fileName + File.separator + childFile.getName(), zipOut);
+                }
+            }
+            return;
+        }
+        try (FileInputStream fis = new FileInputStream(fileToZip)) {
+            ZipEntry zipEntry = new ZipEntry(fileName);
+            zipOut.putNextEntry(zipEntry);
+            byte[] bytes = new byte[1024];
+            int length;
+            while ((length = fis.read(bytes)) >= 0) {
+                zipOut.write(bytes, 0, length);
+            }
+        }
+    }
+}
