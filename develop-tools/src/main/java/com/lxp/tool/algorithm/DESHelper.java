19d86335b8500373bacc3891dbe56f64938a6b79
Super Li <xupenglee@gmail.com>
2023-11-05 22:44:41 +0800

    code clean

--- develop-tools/src/main/java/com/lxp/tool/algorithm/DESHelper.java
+++ develop-tools/src/main/java/com/lxp/tool/algorithm/DESHelper.java
@@ -1,5 +1,6 @@
 package com.lxp.tool.algorithm;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
@@ -9,31 +10,30 @@ import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 
 /**
- * http://www.cnblogs.com/pengxl/p/3967040.html
+ * <a href="http://www.cnblogs.com/pengxl/p/3967040.html">...</a>
  */
 public class DESHelper {
     private static final String DES = "DES";
     private static final String DES_CBC_PKCS5_PADDING = "DES/CBC/PKCS5Padding";
-    private static final String UTF_8 = "UTF-8";
 
     public static String encrypt(String message, String key) throws Exception {
         Cipher cipher = Cipher.getInstance(DES_CBC_PKCS5_PADDING);
-        IvParameterSpec iv = new IvParameterSpec(key.getBytes(UTF_8));
+        IvParameterSpec iv = new IvParameterSpec(key.getBytes(StandardCharsets.UTF_8));
         cipher.init(Cipher.ENCRYPT_MODE, generateSecretKey(key), iv);
-        return Base64.getEncoder().encodeToString(cipher.doFinal(message.getBytes(UTF_8)));
+        return Base64.getEncoder().encodeToString(cipher.doFinal(message.getBytes(StandardCharsets.UTF_8)));
     }
 
     public static String decrypt(String message, String key) throws Exception {
         byte[] bytes = Base64.getDecoder().decode(message);
         Cipher cipher = Cipher.getInstance(DES_CBC_PKCS5_PADDING);
-        IvParameterSpec iv = new IvParameterSpec(key.getBytes(UTF_8));
+        IvParameterSpec iv = new IvParameterSpec(key.getBytes(StandardCharsets.UTF_8));
         cipher.init(Cipher.DECRYPT_MODE, generateSecretKey(key), iv);
         byte[] retByte = cipher.doFinal(bytes);
         return new String(retByte);
     }
 
     private static SecretKey generateSecretKey(String key) throws Exception {
-        DESKeySpec desKeySpec = new DESKeySpec(key.getBytes(UTF_8));
+        DESKeySpec desKeySpec = new DESKeySpec(key.getBytes(StandardCharsets.UTF_8));
         SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
         return keyFactory.generateSecret(desKeySpec);
     }
--- develop-tools/src/main/java/com/lxp/tool/pdf/MergePdfHelper.java
+++ develop-tools/src/main/java/com/lxp/tool/pdf/MergePdfHelper.java
@@ -1,26 +1,24 @@
 package com.lxp.tool.pdf;
 
-import java.io.File;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfCopy;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfSmartCopy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.pdf.PdfCopy;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfSmartCopy;
-
 public class MergePdfHelper {
     private static final Logger LOGGER = LoggerFactory.getLogger(MergePdfHelper.class);
 
     public static void mergePdfViaIText(List<InputStream> inputStreams, String destinationPath) {
-        try (OutputStream mergeOutputStream = new FileOutputStream(new File(destinationPath))) {
+        try (OutputStream mergeOutputStream = new FileOutputStream(destinationPath)) {
             Document document = new Document();
             PdfCopy copy = new PdfSmartCopy(document, mergeOutputStream);
             document.open();
