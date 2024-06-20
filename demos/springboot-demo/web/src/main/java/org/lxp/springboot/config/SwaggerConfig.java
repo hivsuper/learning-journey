3686d4cd65fc2c31a34d3b2919cb42cd8160d0b1
Super Li <xupenglee@gmail.com>
2024-06-20 22:09:35 +0800

    Add SwaggerConfig

---
+++ demos/springboot-demo/web/src/main/java/org/lxp/springboot/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package org.lxp.springboot.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info().title("Web Swagger").contact(new Contact())
+                .description("Web Swagger Description").version("v1")
+        );
+    }
+}
