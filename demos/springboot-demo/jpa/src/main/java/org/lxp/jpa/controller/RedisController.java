16ff399ad22952ff33eb6f3a4b656b84e31ac741
Super Li <xupenglee@gmail.com>
2024-07-21 19:16:15 +0800

    code clean

--- demos/springboot-demo/jpa/src/main/java/org/lxp/jpa/controller/RedisController.java
+++ demos/springboot-demo/jpa/src/main/java/org/lxp/jpa/controller/RedisController.java
@@ -1,5 +1,6 @@
 package org.lxp.jpa.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -8,27 +9,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
-
 @RestController
 @RequestMapping("/redis")
+@RequiredArgsConstructor
 public class RedisController {
-    @Inject
-    private StringRedisTemplate stringRedisTemplate;
+    private final StringRedisTemplate stringRedisTemplate;
 
     @PostMapping("/add")
-    public ResponseEntity<Void> set(String key, String value) throws Exception {
+    public ResponseEntity<Void> set(String key, String value) {
         stringRedisTemplate.opsForValue().set(key, value);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/get")
-    public ResponseEntity<String> get(String key) throws Exception {
+    public ResponseEntity<String> get(String key) {
         return ResponseEntity.ok(stringRedisTemplate.opsForValue().get(key));
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<Boolean> delete(String key) throws Exception {
+    public ResponseEntity<Boolean> delete(String key) {
         return ResponseEntity.ok(stringRedisTemplate.delete(key));
     }
 }
