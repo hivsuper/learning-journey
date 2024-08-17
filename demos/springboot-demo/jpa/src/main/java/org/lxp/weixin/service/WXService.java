b5aa7d7f81ab62fa0fda66bff197c80a95f039b5
Super Li <xupenglee@gmail.com>
2024-08-17 16:06:42 +0800

    Revert "Remove wx.getUserProfile from 561bdd710065ccb31afaa2ba2ee4cb1547b6109f since it's not been supported anymore"
    This reverts commit 67bad1857c124c21ac89673599b594389ab49b59.

--- demos/springboot-demo/jpa/src/main/java/org/lxp/weixin/service/WXService.java
+++ demos/springboot-demo/jpa/src/main/java/org/lxp/weixin/service/WXService.java
@@ -5,12 +5,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.lxp.weixin.exception.WXException;
 import org.lxp.weixin.response.Jscode2sessionResponse;
+import org.lxp.weixin.response.UserInfoResponse;
+import org.lxp.weixin.util.AesCbcUtil;
 import org.lxp.weixin.util.JsonHelper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 @Slf4j
@@ -32,12 +35,18 @@ public class WXService {
                         "&secret=" + secret + "&js_code=" + jsCode + "&grant_type=authorization_code")
                 .retrieve()
                 .bodyToMono(String.class);
-        String sessionId = jscode2sessionMono.block();
-        log.debug("sessionId={}", sessionId);
-        Jscode2sessionResponse response = JsonHelper.toObject(Jscode2sessionResponse.class, sessionId);
+        String sesseionId = jscode2sessionMono.block();
+        log.debug("sessionId={}", sesseionId);
+        Jscode2sessionResponse response = JsonHelper.toObject(Jscode2sessionResponse.class, sesseionId);
         if (Objects.nonNull(response.getErrCode())) {
             throw new WXException(response.getErrCode(), response.getErrMsg());
         }
         return response;
+    }
+
+    public UserInfoResponse getUserInfo(String encryptedData, String iv, String jsCode) throws JsonProcessingException {
+        String userInfo = AesCbcUtil.decrypt(encryptedData, get(jsCode).getSessionKey(), iv, StandardCharsets.UTF_8.name());
+        log.info("decryptedData: {}", userInfo);
+        return JsonHelper.toObject(UserInfoResponse.class, userInfo);
     }
 }
