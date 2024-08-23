20a46f3b02d048385d92334dd47b645ca086432d
Super Li <xupenglee@gmail.com>
2024-08-24 02:38:09 +0800

    Collect classic cases for ParameterizedTest

--- develop-tools/README.md
+++ develop-tools/README.md
@@ -1,7 +1,7 @@
 # develop-tools
 Test Environment:
-- [openjdk version "17" 2021-09-14`](https://download.java.net/openjdk/jdk17/ri/openjdk-17+35_windows-x64_bin.zip)
-- `apache maven 3.9.2` in `IntelliJ IDEA 2023.2.3`
+- [openjdk version "21.0.2" 2024-01-16](https://download.java.net/openjdk/jdk21/ri/openjdk-21+35_windows-x64_bin.zip)
+- `apache maven 3.9.6` in `IntelliJ IDEA 2024.1`
 ----------------------------------------------------
 Common tools help to develop
 - algorithm
---
+++ develop-tools/src/test/java/com/lxp/tool/ParameterizedTests.java
@@ -0,0 +1,54 @@
+package com.lxp.tool;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.time.Month;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ParameterizedTests {
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"  ", "\t", "\n"})
+    void isBlank_ShouldReturnTrueForEmptyStrings(String input) {
+        assertTrue(StringUtils.isBlank(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"test:test", "tEst:test", "Java:java"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(String input, String expected) {
+        String actualValue = input.toLowerCase();
+        assertEquals(expected, actualValue);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsForIsBlank")
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input, boolean expected) {
+        assertEquals(expected, StringUtils.isBlank(input));
+    }
+
+    private static Stream<Arguments> provideStringsForIsBlank() {
+        return Stream.of(
+                Arguments.of(null, true),
+                Arguments.of("", true),
+                Arguments.of("  ", true),
+                Arguments.of("not blank", false)
+        );
+    }
+
+    @ParameterizedTest(name = "{index} {0} is 30 days long")
+    @EnumSource(value = Month.class, names = {"APRIL", "JUNE", "SEPTEMBER", "NOVEMBER"})
+    void someMonths_Are30DaysLong(Month month) {
+        final boolean isALeapYear = false;
+        assertEquals(30, month.length(isALeapYear));
+    }
+}
