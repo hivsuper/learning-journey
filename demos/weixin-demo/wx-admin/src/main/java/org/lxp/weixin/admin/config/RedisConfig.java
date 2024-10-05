6f65306f1c93446e71564477da2c4f648aa30020
Super Li <xupenglee@gmail.com>
2024-10-05 17:05:01 +0800

    weixin-demo: Fix the issue on serialization/deserialization for redis

--- demos/weixin-demo/wx-admin/src/main/java/org/lxp/weixin/admin/config/RedisConfig.java
+++ demos/weixin-demo/wx-admin/src/main/java/org/lxp/weixin/admin/config/RedisConfig.java
@@ -1,15 +1,21 @@
 package org.lxp.weixin.admin.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.security.jackson2.CoreJackson2Module;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
     public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
-        return new GenericJackson2JsonRedisSerializer();
+        final var mapper = new ObjectMapper();
+        mapper.registerModule(new CoreJackson2Module()); // Fix "no Creators, like default construct" exception
+        mapper.registerModule(new JavaTimeModule());
+        return new GenericJackson2JsonRedisSerializer(mapper);
     }
 }
