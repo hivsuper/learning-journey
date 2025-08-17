5301a6ed73b1521cc8729c96496211eb7d8cd930
Super Li <xupenglee@gmail.com>
2025-08-18 01:12:26 +0800

    gradle-demo: Add test

--- demos/gradle-demo/gradle-service/src/main/java/org/lxp/gradle/service/TestTableService.java
+++ demos/gradle-demo/gradle-service/src/main/java/org/lxp/gradle/service/TestTableService.java
@@ -3,8 +3,11 @@ package org.lxp.gradle.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.lxp.gradle.repository.TestTableRepository;
+import org.lxp.gradle.schedule.MDCTaskDecorator;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
@@ -14,10 +17,12 @@ public class TestTableService {
 
     @Async
     public void findAll() {
-        log.info("Find all in TestTable {}", testTableRepository);
+        log.info("@Async find all in TestTable {}", testTableRepository.findAll());
     }
 
     public void findAllSync() {
-        log.info("Find all in TestTable {}", testTableRepository);
+        final var decorator = new MDCTaskDecorator();
+        final var runnable = decorator.decorate(() -> log.info("Find all in TestTable {}", testTableRepository.findAll()));
+        CompletableFuture.runAsync(runnable);
     }
 }
